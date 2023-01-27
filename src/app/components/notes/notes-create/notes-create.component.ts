bf6c1ef6971861c674cd76748f012f8ef8c33ffb
Navinda Chandrasiri <adilanga@gmail.com>
2023-01-27 21:46:20 +0530

    Note Create View Impl

---
+++ src/app/components/notes/notes-create/notes-create.component.ts
@@ -0,0 +1,37 @@
+import {Component, OnInit} from '@angular/core';
+import {Note, NoteCreate} from "../../../models/note.model";
+import {NotesService} from "../../../services/notes.service";
+import {Router} from "@angular/router";
+
+@Component({
+  selector: 'app-notes-create',
+  templateUrl: './notes-create.component.html',
+  styleUrls: ['./notes-create.component.css']
+})
+export class NotesCreateComponent implements OnInit {
+
+  createNoteRequest: NoteCreate = {
+    title: '',
+    description: '',
+    favorite: false
+  }
+
+  constructor(private noteService: NotesService, private router: Router) {
+  }
+
+  ngOnInit(): void {
+  }
+
+  createNote() {
+    console.log(this.createNoteRequest);
+    this.noteService.createNote(this.createNoteRequest).subscribe({
+      next: (response) => {
+        console.log(response)
+        this.router.navigate(['notes-list'])
+      },
+      error: (err) => {
+        console.log(err)
+      }
+    });
+  }
+}
