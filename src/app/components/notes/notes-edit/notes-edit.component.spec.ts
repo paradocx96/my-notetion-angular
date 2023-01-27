4cb93f7072fa4205094f162f79d9dce51484dbc3
Navinda Chandrasiri <adilanga@gmail.com>
2023-01-27 21:53:14 +0530

    Note Edit Init

---
+++ src/app/components/notes/notes-edit/notes-edit.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { NotesEditComponent } from './notes-edit.component';
+
+describe('NotesEditComponent', () => {
+  let component: NotesEditComponent;
+  let fixture: ComponentFixture<NotesEditComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ NotesEditComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(NotesEditComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
