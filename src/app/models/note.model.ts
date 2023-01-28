adc29f036cc4c0d76fbeeec6b48023d16339b2db
Navinda Chandrasiri <adilanga@gmail.com>
2023-01-28 09:57:59 +0530

    Note Model update

--- src/app/models/note.model.ts
+++ src/app/models/note.model.ts
@@ -7,8 +7,19 @@ export interface Note {
   created: string
 }
 
-export interface NoteCreate {
+export interface NoteCreateUpdate {
   title: string,
   description: string,
   favorite: boolean,
+}
+
+export interface NoteSingle {
+  _id: string,
+  title: string,
+  description: string,
+  favorite: boolean,
+  author: string,
+  created: string,
+  createdAt: string,
+  updatedAt: string
 }
