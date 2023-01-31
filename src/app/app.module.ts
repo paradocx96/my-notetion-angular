de568a6570b8ce0b6d61926169c0d99aa79124c4
Navinda Chandrasiri <adilanga@gmail.com>
2023-01-31 15:14:20 +0530

    NgRx Init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,18 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { NotesListComponent } from './components/notes/notes-list/notes-list.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {NotesListComponent} from './components/notes/notes-list/notes-list.component';
 import {HttpClientModule} from "@angular/common/http";
-import { NotesCreateComponent } from './components/notes/notes-create/notes-create.component';
+import {NotesCreateComponent} from './components/notes/notes-create/notes-create.component';
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
-import { NotesEditComponent } from './components/notes/notes-edit/notes-edit.component';
-import { NavigationBarComponent } from './components/navigation/navigation-bar/navigation-bar.component';
-import { ErrorPageComponent } from './components/status/error-page/error-page.component';
+import {NotesEditComponent} from './components/notes/notes-edit/notes-edit.component';
+import {NavigationBarComponent} from './components/navigation/navigation-bar/navigation-bar.component';
+import {ErrorPageComponent} from './components/status/error-page/error-page.component';
+import {StoreModule} from '@ngrx/store';
+import {EffectsModule} from '@ngrx/effects';
+import {authReducer} from "./state/auth/auth.reducer";
 
 @NgModule({
   declarations: [
@@ -25,9 +28,12 @@ import { ErrorPageComponent } from './components/status/error-page/error-page.co
     AppRoutingModule,
     HttpClientModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    StoreModule.forRoot({auth: authReducer}, {}),
+    EffectsModule.forRoot([]),
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
